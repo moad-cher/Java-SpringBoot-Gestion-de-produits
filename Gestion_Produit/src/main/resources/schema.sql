9bdf0c68150282f91c741b6a5b0d578a8a41e8a2
moad-cher <moadchergui13@gmail.com>
2025-12-09 03:21:14 +0000

    .

--- Gestion_Produit/src/main/resources/schema.sql
+++ Gestion_Produit/src/main/resources/schema.sql
@@ -1,40 +1,105 @@
-CREATE DATABASE IF NOT EXISTS tp;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: localhost    Database: tp
+-- ------------------------------------------------------
+-- Server version	8.0.40
 
-USE tp;
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-CREATE TABLE IF NOT EXISTS user (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255),
-    prenom VARCHAR(255),
-    email VARCHAR(255) UNIQUE,
-    password VARCHAR(255),
-    enabled BOOLEAN DEFAULT TRUE
-);
+--
+-- Table structure for table `hibernate_sequence`
+--
 
-CREATE TABLE IF NOT EXISTS produit (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    nom VARCHAR(255),
-    prix DECIMAL(10, 2),
-    devise VARCHAR(10),
-    taxe DECIMAL(10, 2),
-    date_expiration DATE,
-    fournisseur VARCHAR(255),
-    image VARCHAR(255)
-);
+DROP TABLE IF EXISTS `hibernate_sequence`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `hibernate_sequence` (
+  `next_val` bigint DEFAULT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
--- Insert test user
-INSERT IGNORE INTO
-    user (
-        nom,
-        prenom,
-        email,
-        password,
-        enabled
-    )
-VALUES (
-        'Chergui',
-        'Moad',
-        'moadchergui13@gmail.com',
-        '$2a$10$encrypted_password_here',
-        TRUE
-    );
+--
+-- Dumping data for table `hibernate_sequence`
+--
+
+/*!40000 ALTER TABLE `hibernate_sequence` DISABLE KEYS */;
+INSERT INTO `hibernate_sequence` VALUES (3);
+/*!40000 ALTER TABLE `hibernate_sequence` ENABLE KEYS */;
+
+--
+-- Table structure for table `produits`
+--
+
+DROP TABLE IF EXISTS `produits`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `produits` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `date_expiration` datetime DEFAULT NULL,
+  `devise` varchar(3) DEFAULT NULL,
+  `fournisseur` varchar(255) DEFAULT NULL,
+  `image` varchar(255) DEFAULT NULL,
+  `nom` varchar(255) DEFAULT NULL,
+  `prix` double NOT NULL,
+  `taxe` int DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `produits`
+--
+
+/*!40000 ALTER TABLE `produits` DISABLE KEYS */;
+INSERT INTO `produits` VALUES (1,'2025-12-28 00:00:00','EUR','abdellah',NULL,'book',20,0),(2,'2026-02-02 00:00:00','EUR','moad','','pc',8500,0);
+/*!40000 ALTER TABLE `produits` ENABLE KEYS */;
+
+--
+-- Table structure for table `utilisateur`
+--
+
+DROP TABLE IF EXISTS `utilisateur`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `utilisateur` (
+  `id` bigint NOT NULL,
+  `email` varchar(45) NOT NULL,
+  `nom` varchar(20) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `prenom` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `UK_rma38wvnqfaf66vvmi57c71lo` (`email`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `utilisateur`
+--
+
+/*!40000 ALTER TABLE `utilisateur` DISABLE KEYS */;
+INSERT INTO `utilisateur` VALUES (2,'moadchergui13@gmail.com','moad','$2a$10$2FUIqItafsd3dkdgIiVW1.aSEqs9QWP3CFfqEHIBFU22crusC0ZNu','moad');
+/*!40000 ALTER TABLE `utilisateur` ENABLE KEYS */;
+
+--
+-- Dumping routines for database 'tp'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-12-09  3:21:07
